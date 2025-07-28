4eadb2b9a5475b7350b703becececf2371b37f13
Joseph Seitaro White <seitaro.white@gmail.com>
2025-07-28 23:16:44 +0900

    Added place emojis

---
+++ src/features/markers/placeEmojis.js
@@ -0,0 +1,20 @@
+// Emojis to use for place markers
+export const placeEmojis = {
+    "スーパー": "🛒",         // Supermarket
+    "ホテル": "🏨",           // Hotel
+    "交通": "🚉",             // Transport (station)
+    "公園": "🏞️",            // Park
+    "博物館": "🏛️",          // Museum
+    "喫茶店": "☕️",           // Cafe
+    "店": "🏬",               // Shop
+    "料理店": "🍽️",          // Restaurant
+    "横丁": "🏮",             // Alley/Bar street
+    "歴史の場所": "🏯",        // Historical place
+    "神社とお寺": "⛩️",       // Shrine/Temple
+    "自然": "🌳",             // Nature
+    "自転車駐輪場": "🚲",      // Bicycle parking
+    "船": "⛴️",               // Ship/Ferry
+    "行きたい店": "⭐",         // Want to visit (shop)
+    "面白いもの": "🎲",        // Interesting thing
+    "飲み屋": "🍶"             // Nomiya
+}
