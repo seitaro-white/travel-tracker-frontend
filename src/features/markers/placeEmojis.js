750b35cb3d62a6a51be503c4eb773962bb56a262
Joseph Seitaro White <seitaro.white@gmail.com>
2026-08-02 16:47:20 +0100

    More places

--- src/features/markers/placeEmojis.js
+++ src/features/markers/placeEmojis.js
@@ -1,20 +1,39 @@
 // Emojis to use for place markers
 export const placeEmojis = {
+    "駅": "🚉",            // Admin
+    "自然": "🌲",            // Admin
+    "アイスクリーム": "🍦",     // Admin
+    "イタリア": "🍝",            // Admin
+    "食堂": "🍱",            // Admin
+    "焼き鳥": "🐔",            // Admin
+    "飲み屋": "🍻",            // Admin
+    "ラーメン": "🍜",            // Admin
+    "Admin": "📑",            // Admin
+    "店": "🛍️",            // Admin
+    "ピザ": "🍕",            // Admin
+    "温泉": "♨️",            // Admin
+    "トンアシア": "🥗",            // Admin
+    "公園": "⛲️",            // Admin
+    "寿司": "🍣",            // Admin
+    "中華": "🥟",            // Admin
+    "焼肉": "🥩",            // Admin
+    "カレー": "🍛",            // Admin
+    "Venues": "🎵",            // Admin
+    "山": "⛰️",            // Admin
+    "韓食": "🌶️",            // Admin
+    "ファスト": "🍔",            // Admin
+    "Sports": "🏐",            // Admin
     "スーパー": "🛒",         // Supermarket
     "ホテル": "🏨",           // Hotel
     "交通": "🚉",             // Transport (station)
     "公園": "⛲️",            // Park
     "博物館": "🏛️",          // Museum
     "喫茶店": "☕️",           // Cafe
-    "店": "🛍️",               // Shop
     "料理店": "🍽️",          // Restaurant
     "横丁": "🏮",             // Alley/Bar street
     "歴史の場所": "🏯",        // Historical place
     "神社とお寺": "⛩️",       // Shrine/Temple
-    "自然": "🌳",             // Nature
     "自転車駐輪場": "🚲",      // Bicycle parking
     "船": "⛴️",               // Ship/Ferry
-    "行きたい店": "⭐",         // Want to visit (shop)
-    "面白いもの": "🎲",        // Interesting thing
-    "飲み屋": "🍶"             // Nomiya
+    "居酒屋": "🍶"             // Izakaya
 }
